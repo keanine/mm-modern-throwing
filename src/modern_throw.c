0b323960b311481297981131244a8e8e49405a28
keanine <keanen195@gmail.com>
2025-05-24 12:04:54 +0100

    Reverted C button actions

--- src/modern_throw.c
+++ src/modern_throw.c
@@ -14,7 +14,7 @@ extern PlayerAnimationHeader* D_8085BE84[PLAYER_ANIMGROUP_MAX][PLAYER_ANIMTYPE_M
 RECOMP_PATCH s32 Player_ActionHandler_9(Player* this, PlayState* play) {
     if (this->stateFlags1 & PLAYER_STATE1_CARRYING_ACTOR) {
         if (this->heldActor != NULL) {
-                if (CHECK_BTN_ANY(play->state.input->press.button, BTN_CRIGHT | BTN_CLEFT | BTN_CDOWN | BTN_A)) {
+                if (CHECK_BTN_ANY(play->state.input->press.button, BTN_A)) {
                     if (!func_808313A8(play, this, this->heldActor)) {
                         func_8083D6DC(this, play);
                     }
@@ -26,6 +26,18 @@ RECOMP_PATCH s32 Player_ActionHandler_9(Player* this, PlayState* play) {
                         Player_Anim_PlayOnce(play, this, D_8085BE84[PLAYER_ANIMGROUP_put][this->modelAnimType]);
                         return true;
                     }
+                }
+                else if (CHECK_BTN_ANY(play->state.input->press.button, BTN_CRIGHT | BTN_CLEFT | BTN_CDOWN)) {
+                    if (!func_808313A8(play, this, this->heldActor)) {
+                        if (!Player_CanThrowCarriedActor(this, this->heldActor)) {
+                            Player_SetAction(play, this, Player_Action_41, 1);
+                            Player_Anim_PlayOnce(play, this, D_8085BE84[PLAYER_ANIMGROUP_put][this->modelAnimType]);
+                            return true;
+                        }
+                        func_8083D6DC(this, play);
+                    }
+
+                    return true;
                 }
         }
     }
